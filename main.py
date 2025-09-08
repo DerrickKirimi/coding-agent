d53de6dad687122d2c55a0a352ffe242e325fcb0
DerrickKirimi <derrickkirimi.dev@gmail.com>
2025-09-08 23:52:55 +0300

    Access token count

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-load_dotenv()
-api_key=os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key=os.environ.get("GEMINI_API_KEY")
 
-response = client.models.generate_content (
-    model = "gemini-2.0-flash-001",
-    contents = """
-What is your context size, explain
-"""
-)
-print(response.text)
+    client = genai.Client(api_key=api_key)
 
+    response = client.models.generate_content (
+        model = "gemini-2.0-flash-001",
+        contents = """What is your context size, explain"""
+    )
+    print(response.text)
+
+    if response is None or response.usage_metadata is None:
+        print("Response is malformed")
+    print(f"Prompt token count:{response.usage_metadata.prompt_token_count}")
+    print(f"Response token count: {response.usage_metadata.candidates_token_count}")
+
+if __name__=="__main__":
+    main()
