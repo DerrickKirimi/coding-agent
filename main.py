50852a8f5c3b952ea61bbcaf8d7b69684618331c
DerrickKirimi <derrickkirimi.dev@gmail.com>
2025-09-08 17:38:43 +0300

    add client

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from coding-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
+load_dotenv()
+api_key=os.environ.get("GEMINI_API_KEY")
 
+client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+response = client.models.generate_content (
+    model = "gemini-2.0-flash-001",
+    contents = """
+What is your context size, explain
+"""
+)
+print(response.text)
+
